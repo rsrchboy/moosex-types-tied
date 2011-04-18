be69573dfe6420e9499180c1be694376c41da741
Chris Weyl <cweyl@alumni.drew.edu>
2011-04-17 19:17:32 -0700

    initial commit

---
+++ lib/MooseX/Types/Tied.pm
@@ -0,0 +1,10 @@
+package MooseX::Types::Tied;
+
+# ABSTRACT: The great new MooseX::Types::Tied!
+
+use Moose;
+use namespace::autoclean;
+use common::sense;
+
+
+__PACKAGE__->meta->make_immutable;
---
+++ t/00-load.t
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use Test::More tests => 1;
+
+BEGIN { use_ok '' }
+
+diag("Testing MooseX-Types-Tied $Dist::Zilla::Plugin::lib/MooseX/Types/Tied.pm::VERSION, Perl $], $^X");
